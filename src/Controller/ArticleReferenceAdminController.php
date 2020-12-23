6e0a95bdfc2280be8f88bff9450cf9d168689b84
Stefan Starkbaum <s.starkbaum@austrian-standards.at>
2020-12-23 16:56:14 +0100

    Chapter 22: mime type validation

--- src/Controller/ArticleReferenceAdminController.php
+++ src/Controller/ArticleReferenceAdminController.php
@@ -9,8 +9,13 @@ use App\Entity\ArticleReference;
 use App\Service\UploaderHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ArticleReferenceAdminController extends BaseController
 {
@@ -20,15 +25,54 @@ class ArticleReferenceAdminController extends BaseController
      * @param Request $request
      * @param UploaderHelper $uploaderHelper
      * @param EntityManagerInterface $entityManager
+     * @param ValidatorInterface $validator
+     * @return RedirectResponse
      */
     public function uploadArticleReference(
         Article $article,
         Request $request,
         UploaderHelper $uploaderHelper,
-        EntityManagerInterface $entityManager)
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator
+    )
     {
         /** @var UploadedFile $uploadedFile */
         $uploadedFile = $request->files->get('reference');
+
+        $violations = $validator->validate(
+            $uploadedFile,
+            [
+                new File([
+                    'maxSize' => '5M',
+                    'mimeTypes' => [
+                        'image/*',
+                        'application/pdf',
+                        'application/msword',
+                        'application/vnd.ms-excel',
+                        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                        'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+                        'text/plain'
+                    ],
+                ]),
+                new NotNull([
+                    'message' => 'Please select a file to upload!',
+                ]),
+            ]
+        );
+
+        if ($violations->count() > 0) {
+
+            /** @var ConstraintViolation $violation */
+            $violation = $violations[0];
+
+            $this->addFlash('error', $violation->getMessage());
+
+            return $this->redirectToRoute('admin_article_edit', [
+                'id' => $article->getId(),
+            ]);
+
+        }
 
         $filename = $uploaderHelper->uploadArticleReference($uploadedFile);
 
